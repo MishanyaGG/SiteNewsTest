6fcf3ae463b777489a8df7f06166204bc3d82120
MishanyaGG <mischatitovmail.ru@gmail.com>
2026-08-25 19:49:24 +0300

    Изменение логика подключения бд для работы на других пк с проектом

--- includes/config.php
+++ includes/config.php
@@ -1,9 +1,10 @@
 <?php
+$env = parse_ini_file(__DIR__ . '\..\.env');
 // Данные для Beget берутся из панели: «Базы данных» → имя/логин/пароль
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'mischa3h_shop');   // на Beget: логин_имяБД
-define('DB_USER', 'mischa3h_shop');
-define('DB_PASS', 'x8&7I7VuWE*E');
+define('DB_HOST', $env['DB_HOST']);
+define('DB_NAME', $env['DB_NAME']);   // на Beget: логин_имяБД
+define('DB_USER', $env['DB_USER']);
+define('DB_PASS', $env['DB_PASS']); //x8&7I7VuWE*E
 
 define('SITE_NAME', 'ShopLite');
 define('UPLOAD_DIR', __DIR__ . '/../uploads/');
